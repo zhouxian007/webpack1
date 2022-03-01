68dfde8d6f412ecc58ed01b32bd249d0efe9b989
zhouxian <726839037@qq.com>
2022-03-02 00:02:31 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'development',
@@ -13,16 +13,42 @@ module.exports = {
  
     filename: 'index.[contenthash].js',
   },
-  plugins: [new HtmlWebpackPlugin({
+  plugins: [
+  new HtmlWebpackPlugin({
     title:'aaa',
     template:'src/assets/index.html'
 
-  })],
+  }),
+  new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+      ignoreOrder:false,
+    }),
+  ],
+  
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/i,
+  //       // use: ["style-loader", "css-loader"],
+  //       use:[
+  //         {
+  //           loader: MiniCssExtractPlugin.loader,
+  //           options:{
+  //             publicPath:'../',
+  //             hmr:process.env.NODE_ENV ==='development',
+  //           },
+  //         },
+  //         'css-loader',
+  //       ]
+  //     },
+  //   ],
+  // },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
