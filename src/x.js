77e560a6d6b83b697e263be0b7f3fb8b744783b0
zhouxian <726839037@qq.com>
2022-03-01 23:42:25 +0800

    updata

--- src/x.js
+++ src/x.js
@@ -1,1 +1,2 @@
 export default 'xxx'
+import './x.css'
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+
 
 module.exports = {
   mode: 'development',
+ 
+  devServer: {
+    static: "./dist"
+  },
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name][contenthash].js',}
+ 
+    filename: 'index.[contenthash].js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title:'aaa',
+    template:'src/assets/index.html'
+
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
